87b2861383890056a8ddf886eaa0ca6986ed399c
ProkhorovKV <ProkhorovKV@Piteravto.ru>
2022-01-10 20:16:40 +0300

    Загружается карта

--- app/src/main/java/ru/piteravto/osmroutetest/map/MapFragment.kt
+++ app/src/main/java/ru/piteravto/osmroutetest/map/MapFragment.kt
@@ -1,11 +1,16 @@
 package ru.piteravto.osmroutetest.map
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import ru.piteravto.osmroutetest.App
 import ru.piteravto.osmroutetest.databinding.FragmentMapBinding
+import java.io.File
 
 class MapFragment: Fragment() {
     private var _binding: FragmentMapBinding? = null
@@ -16,12 +21,31 @@ class MapFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        initMapConfiguration()
         _binding = FragmentMapBinding.inflate(inflater, container, false)
+
+
         return binding.root
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private fun initMapConfiguration() {
+        val context = App.context
+        val osmDroidConfig = Configuration.getInstance()
+        val baseOsmDroidFolder = File(context.cacheDir, "osmdroid")
+        osmDroidConfig.osmdroidBasePath = baseOsmDroidFolder
+        val tileCacheFolder = File(baseOsmDroidFolder, "tiles")
+        osmDroidConfig.osmdroidTileCache = tileCacheFolder
 
+        osmDroidConfig.load(
+            context,
+            PreferenceManager.getDefaultSharedPreferences(context)
+        )
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val mapView = binding.map
+        mapView.setTileSource(TileSourceFactory.MAPNIK)
     }
 }
