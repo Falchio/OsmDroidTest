5de8a57d148012ef90a5c4fc8ed1f655d641e1d3
ProkhorovKV <ProkhorovKV@Piteravto.ru>
2022-01-10 19:11:55 +0300

    Добавлена внутренняя навигация.

--- app/src/main/java/ru/piteravto/osmroutetest/MainActivity.kt
+++ app/src/main/java/ru/piteravto/osmroutetest/MainActivity.kt
@@ -2,10 +2,24 @@ package ru.piteravto.osmroutetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
+import ru.piteravto.osmroutetest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: NavigationView = binding.navView
+        navController = findNavController(R.id.nav_host_fragment_content_main)
+        navView.setupWithNavController(navController)
     }
 }
